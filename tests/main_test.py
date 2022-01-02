72137ae6d5a1a6199c032fea50289e1cd5a3a6e8
Viacheslav <slava.k@ks2corp.com>
2022-01-02 21:14:14 +0300

    First commit

---
+++ tests/main_test.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+
+import app.example as exmpl
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_upper(self):
+      self.assertEqual('foo'.upper(), 'FOO')
+
+  def test_isupper(self):
+      self.assertTrue('FOO'.isupper())
+      self.assertFalse('Foo'.isupper())
+
+  def test_split(self):
+      s = 'hello world'
+      self.assertEqual(s.split(), ['hello', 'world'])
+      # Проверим, что s.split не работает, если разделитель - не строка
+      with self.assertRaises(TypeError):
+          s.split(2)
+          
+  def test_main(self):
+      self.assertFalse(exmpl.dupa_printing(12))
+      self.assertTrue(exmpl.dupa_printing("dupa"))
+
+if __name__ == '__main__':
+    unittest.main()
